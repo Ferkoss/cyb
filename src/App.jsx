05c71ac314ffd05f3ce1bb9fbc8d895fe162e906
Fernando Koss <fermkoss@gmail.com>
2024-12-05 02:16:37 -0300

    agregado /cyb

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
 
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: "/cyb",
       element:
         <View>
           <Main />
--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
  // base:"https://ferkoss.github.io/cyb"
-  base:"/cyb/"
+  base:"https://ferkoss.github.io/cyb/"
 })
