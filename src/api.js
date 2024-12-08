b566b190b649aeab6028d351285de2ff63c7ea94
Fernando Koss <fermkoss@gmail.com>
2024-12-08 18:57:49 -0300

    cambio de api url

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const api_base_url="https://localhost:7246"
+export const api_base_url="http://www.cyb-api.somee.com"
 //https://localhost:7246
 //http://localhost:5000
 //http://www.cyb-api.somee.com
