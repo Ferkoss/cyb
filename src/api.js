44b667eaf24bdd287c1b99e5a51fb9b4878d77e7
Fernando Koss <fermkoss@gmail.com>
2024-12-09 00:37:44 -0300

    meta no index

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const api_base_url="http://www.cyb-api.somee.com"
+export const api_base_url="https://www.cyb-api.somee.com"
 //https://localhost:7246
 //http://localhost:5000
 //http://www.cyb-api.somee.com
